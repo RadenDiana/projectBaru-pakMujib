0dd3f7c748f68bb6d957b3a920152525b085c5e0
RadenDiana <rmardianamaulana@gmail.com>
2025-08-09 08:27:21 +0700

    Add files via upload

---
+++ projectbaru.bab7/src/projectbaru/bab7/ProjectbaruBab7.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package projectbaru.bab7;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author ali
+ */
+public class ProjectbaruBab7 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //cara1
+//        String[] nama1;
+//        //cara2
+//        String nama2[];
+//        //cara3
+//        String[] nama3 = new String[2];
+
+//                  String nama[] = new String[3];
+//                  nama[0] = "Raden";
+//                  nama[1] = "Diana";
+//                  nama[2] = "Maulana";
+//                  System.out.println("Index 0 : " + nama[0]);
+//                  System.out.println("Index 1 : " + nama[1]);
+//                  System.out.println("Index 2 : " + nama [2]);
+
+//          String[] nama = new String[3];
+//          nama[0] = JOptionPane.showInputDialog(null, "Nama : ", "index ke-0",JOptionPane.INFORMATION_MESSAGE);
+//          nama[1] = JOptionPane.showInputDialog(null, "Nama : ", "index ke-1", JOptionPane.INFORMATION_MESSAGE);
+//          nama[2] = JOptionPane.showInputDialog(null, "Nama : ", "index ke-2", JOptionPane.INFORMATION_MESSAGE );
+//          JOptionPane.showMessageDialog(null, nama[0]+", "+nama[1]+","+nama[2]);
+  
+//                    String umur[][] = new String[2][2];
+//                    umur[0][0] = "Raden";
+//                    umur[0][1] = "17";
+//                    umur[1][0] = "Ali";
+//                    umur[1][1] = "23";
+//                    System.out.println("Nama : " + umur[0][0]+ "umur : " +umur[0][1]);
+//                    System.out.println("Nama : " + umur[0][1]+ "umur : " +umur[1][1]);
+                    
+    }
+    
+}
