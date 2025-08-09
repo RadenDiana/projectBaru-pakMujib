3fd45378b80a20bd8c8c737ef5fa9b0f927f047b
RadenDiana <rmardianamaulana@gmail.com>
2025-08-09 08:26:48 +0700

    Add files via upload

---
+++ projectbaru.bab6/src/projectbaru/bab6/JOptionpane.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package projectbaru.bab6;
+
+/**
+ *
+ * @author ali
+ */
+class JOptionpane {
+
+    static class JOptionPane {
+
+        public JOptionPane() {
+        }
+    }
+    
+}
